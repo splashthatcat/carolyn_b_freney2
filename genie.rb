f5fe1926d18548a9b2f40f89596af2346c185580
splashthatcat <carolyn.freney@gmail.com>
2017-03-10 20:38:54 -0500

    Add genie.rb

---
+++ genie.rb
@@ -0,0 +1,15 @@
+#Genie Class
+	#Who Is My Genie?
+		#Needs A Name And An Age
+class Genie
+	def initialize (name, age)
+		#Instance Variables
+	@name = name
+	@age = age
+end
+
+#Driver Code
+	#Creating My Genie To Initialize Method With Name And Age
+genie = Genie.new("name", "age")
+genie = []
+genie << Genie.new("Dee", "Immortal")
