db3c32966184760adefb37d5789a90c5b307284d
splashthatcat <carolyn.freney@gmail.com>
2017-03-10 20:51:28 -0500

    Update class in genie.rb

--- genie.rb
+++ genie.rb
@@ -2,6 +2,12 @@
 	#Who Is My Genie?
 		#Needs A Name And An Age
 class Genie
+	#Also Need To Update The Class:
+			#So We Can Access The Age (But Not Change It)
+			#And Access AND Change The Name
+	attr_accessor : name
+	attr_reader :age
+	
 	def initialize (name, age)
 		#Instance Variables
 	@name = name
@@ -10,6 +16,7 @@ end
 
 #Driver Code
 	#Creating My Genie To Initialize Method With Name And Age
+		#Should Be Updated To Reflect Updates To The Class
 genie = Genie.new("name", "age")
 genie = []
 genie << Genie.new("Dee", "Immortal")
